c56f4318ac53708af5fcdcf67db356b552eb9603
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2025-03-01 10:54:53 -0300

    :bricks: ci: Generate Dockerfile and first continuos integration

--- test/factories/make-client.ts
+++ test/factories/make-client.ts
@@ -1,9 +1,9 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Client, ClientProps } from "@/domain/register/enterprise/entities/client";
-// import { PrismaClientsMapper } from "@/infra/database/prisma/mappers/prisma-clients-mapper";
-// import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaClientsMapper } from "@/infra/database/prisma/mappers/prisma-clients-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import {faker} from "@faker-js/faker";
-// import { Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 
 export function makeClient(override: Partial<ClientProps> = {}, id?: UniqueEntityID) {
   return Client.create({
@@ -14,20 +14,20 @@ export function makeClient(override: Partial<ClientProps> = {}, id?: UniqueEntit
   }, id)
 }
 
-// @Injectable()
-// export class ClientFactory {
+@Injectable()
+export class ClientFactory {
 
-//   constructor(
-//     private prisma: PrismaService
-//   ){}
+  constructor(
+    private prisma: PrismaService
+  ){}
 
-//   async makePrismaClient(data: Partial<ClientProps> = {}) {
-//     const client = makeClient(data)
+  async makePrismaClient(data: Partial<ClientProps> = {}) {
+    const client = makeClient(data)
 
-//     await this.prisma.client.create({
-//       data: PrismaClientsMapper.toPrisma(client)
-//     })
+    await this.prisma.client.create({
+      data: PrismaClientsMapper.toPrisma(client)
+    })
 
-//     return client
-//   }
-// }
+    return client
+  }
+}
